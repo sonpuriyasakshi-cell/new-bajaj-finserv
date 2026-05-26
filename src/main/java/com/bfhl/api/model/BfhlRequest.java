5622865f0e58d6ade306c70d8dd9f10fd92c11fa
sonpuriyasakshi-cell <sonpuriyasakshi@gmail.com>
2026-05-26 15:53:22 +0530

    Add BfhlRequest.java

---
+++ src/main/java/com/bfhl/api/model/BfhlRequest.java
@@ -0,0 +1,15 @@
+package com.bfhl.api.model;
+
+import java.util.List;
+
+public class BfhlRequest {
+    private List<String> data;
+
+        public List<String> getData() {
+                return data;
+                    }
+
+                        public void setData(List<String> data) {
+                                this.data = data;
+                                    }
+                                    }
